e2667a001b1b5665844e7f3a86d98a62056840f1
Imran Suhaimi <63248465+imrnekko@users.noreply.github.com>
2022-09-14 12:20:59 +0800

    Update email_config.php

--- mail/phpmailer/email_config.php
+++ mail/phpmailer/email_config.php
@@ -1,9 +1,5 @@
 <?php
-/*
-	Authors			: Muhd Eizan Shafiq Bin Abd Aziz, Zazaleena Binti Zakariah, Nursyahidah Binti Alias
-	Date created	: 18 June 2011
-	Description		: phpmailer config
-*/
+
 define("HOST", "smtp.gmail.com");
 define("SMTP_DEBUG", 1);
 define("SMTP_AUTH", true);
