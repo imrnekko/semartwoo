a245792b487aca690cebbbc6d891281a2f53b057
Imran Suhaimi <63248465+imrnekko@users.noreply.github.com>
2022-09-14 12:47:42 +0800

    Delete mail directory

--- mail/PHPMailer-master/email_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-define("HOST", "smtp.gmail.com");
-define("SMTP_DEBUG", 2);
-define("SMTP_AUTH", false);
-define("SMTP_SECURE", "tls");
-define("PORT", 587);
-define("USERNAME", "hello@gmail.com");
-define("PASSWORD", "password");
-?>
